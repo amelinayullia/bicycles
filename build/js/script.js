6020242e63913a8d9d48c39cffc29791e8ed46ad
Amelina_yuliia <amelinayullia@gmail.com>
2020-07-14 22:23:34 +0300

    доделывает правки по ревью

--- build/js/script.js
+++ build/js/script.js
@@ -2,25 +2,23 @@
 var bodyElement = document.querySelector('body');
 var navMain = bodyElement.querySelector('.main-nav');
 var navToggle = bodyElement.querySelector('.main-nav__toggle');
-var mainElement = bodyElement.querySelector('.main');
-var logoElement = bodyElement.querySelector('.header__logo');
+var introElement = bodyElement.querySelector('.intro');
+var logoElement = bodyElement.querySelector('.intro__logo');
 
-if (bodyElement && navMain && navToggle && mainElement && logoElement) {
-  navMain.classList.remove('main-nav--nojs');
-  navMain.classList.remove('main-nav--opened');
-  navMain.classList.add('main-nav--closed');
-  mainElement.classList.remove('main--nojs');
-  logoElement.classList.remove('header__logo--nojs');
+navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav--opened');
+navMain.classList.add('main-nav--closed');
+introElement.classList.remove('intro--nojs');
+logoElement.classList.remove('intro__logo--nojs');
 
-  navToggle.addEventListener('click', function () {
-    if (navMain.classList.contains('main-nav--closed')) {
-      navMain.classList.remove('main-nav--closed');
-      navMain.classList.add('main-nav--opened');
-      bodyElement.classList.add('overflowed');
-    } else {
-      navMain.classList.add('main-nav--closed');
-      navMain.classList.remove('main-nav--opened');
-      bodyElement.classList.remove('overflowed');
-    }
-  });
-}
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+    bodyElement.classList.add('overflowed');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+    bodyElement.classList.remove('overflowed');
+  }
+});
--- source/js/script.js
+++ source/js/script.js
@@ -2,14 +2,14 @@
 var bodyElement = document.querySelector('body');
 var navMain = bodyElement.querySelector('.main-nav');
 var navToggle = bodyElement.querySelector('.main-nav__toggle');
-var mainElement = bodyElement.querySelector('.main');
-var logoElement = bodyElement.querySelector('.header__logo');
+var introElement = bodyElement.querySelector('.intro');
+var logoElement = bodyElement.querySelector('.intro__logo');
 
 navMain.classList.remove('main-nav--nojs');
 navMain.classList.remove('main-nav--opened');
 navMain.classList.add('main-nav--closed');
-mainElement.classList.remove('main--nojs');
-logoElement.classList.remove('header__logo--nojs');
+introElement.classList.remove('intro--nojs');
+logoElement.classList.remove('intro__logo--nojs');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
