215a2d6782be661139e5efebeaaf026ea6fd6520
Amelina_yuliia <amelinayullia@gmail.com>
2020-07-15 19:45:57 +0300

    делает правки после ревью

--- source/js/script.js
+++ source/js/script.js
@@ -1,24 +1,26 @@
-'use strict';
-var bodyElement = document.querySelector('body');
-var navMain = bodyElement.querySelector('.main-nav');
-var navToggle = bodyElement.querySelector('.main-nav__toggle');
-var introElement = bodyElement.querySelector('.intro');
-var logoElement = bodyElement.querySelector('.intro__logo');
+"use strict";
+var bodyElement = document.querySelector("body");
+var navMain = bodyElement.querySelector(".main-nav");
+var navToggle = bodyElement.querySelector(".main-nav__toggle");
+var introElement = bodyElement.querySelector(".intro");
+var logoElement = bodyElement.querySelector(".intro__logo");
 
-navMain.classList.remove('main-nav--nojs');
-navMain.classList.remove('main-nav--opened');
-navMain.classList.add('main-nav--closed');
-introElement.classList.remove('intro--nojs');
-logoElement.classList.remove('intro__logo--nojs');
+if (bodyElement && navMain && navToggle && introElement && logoElement) {
+  navMain.classList.remove("main-nav--nojs");
+  navMain.classList.remove("main-nav--opened");
+  navMain.classList.add("main-nav--closed");
+  introElement.classList.remove("intro--nojs");
+  logoElement.classList.remove("intro__logo--nojs");
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-    bodyElement.classList.add('overflowed');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-    bodyElement.classList.remove('overflowed');
-  }
-});
+  navToggle.addEventListener("click", function () {
+    if (navMain.classList.contains("main-nav--closed")) {
+      navMain.classList.remove("main-nav--closed");
+      navMain.classList.add("main-nav--opened");
+      bodyElement.classList.add("overflowed");
+    } else {
+      navMain.classList.add("main-nav--closed");
+      navMain.classList.remove("main-nav--opened");
+      bodyElement.classList.remove("overflowed");
+    }
+  });
+}
