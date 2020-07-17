422e159ac5507dab76d1c856e305d744b3b8a68a
Amelina_yuliia <amelinayullia@gmail.com>
2020-07-17 10:58:41 +0300

    добавляет разметку и стили блокам контакты и футер

--- build/js/script.js
+++ build/js/script.js
@@ -4,9 +4,11 @@ var navMain = bodyElement.querySelector(".main-nav");
 var navToggle = bodyElement.querySelector(".main-nav__toggle");
 var introElement = bodyElement.querySelector(".intro");
 var logoElement = bodyElement.querySelector(".intro__logo");
+var mapWrapper = document.querySelector(".contacts__map-wrapper");
 
 if (bodyElement && navMain && navToggle && introElement && logoElement) {
   navMain.classList.remove("main-nav--nojs");
+  mapWrapper.classList.remove("contacts__map-wrapper--nojs");
   navMain.classList.remove("main-nav--opened");
   navMain.classList.add("main-nav--closed");
   introElement.classList.remove("intro--nojs");
