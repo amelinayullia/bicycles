e25225f46ac4945ef3e94c064d0f037f0b2a38bc
Amelina_yuliia <amelinayullia@gmail.com>
2020-07-19 16:17:20 +0300

    добавляет валидацию для формы

--- source/js/main.js
+++ source/js/main.js
@@ -5,6 +5,11 @@ var navToggle = bodyElement.querySelector(".main-nav__toggle");
 var introElement = bodyElement.querySelector(".intro");
 var logoElement = bodyElement.querySelector(".intro__logo");
 var mapWrapper = document.querySelector(".contacts__map-wrapper");
+var form = document.querySelector('.intro__form');
+var nameInput = document.querySelector('#input-name');
+var phoneInput = document.querySelector('#input-phone');
+var numInputsRegEx = /[^\d,+,\-,()]/g;
+
 
 if (bodyElement && navMain && navToggle && introElement && logoElement) {
   navMain.classList.remove("main-nav--nojs");
@@ -25,4 +30,35 @@ if (bodyElement && navMain && navToggle && introElement && logoElement) {
       bodyElement.classList.remove("overflowed");
     }
   });
+}
+
+var checkInputValidity = function(input) {
+  if (input.value) {
+    return true;
+  }
+  input.classList.add('error');
+  return false;
+}
+var onInputChangeHandler = function(e) {
+  if (e.target.classList.contains('error')) {
+    e.target.classList.remove('error');
+  }
+}
+var onPhoneInputChangeHandler = function(e) {
+  e.target.value = e.target.value.replace(numInputsRegEx, '');
+}
+var onFormSubmitHandler = function (e) {
+  e.preventDefault();
+  checkInputValidity(nameInput);
+  checkInputValidity(phoneInput);
+  if (checkInputValidity(nameInput) && checkInputValidity(phoneInput)) {
+    form.reset();
+  }
+}
+if (form) {
+  form.noValidate = true;
+  form.addEventListener('submit', onFormSubmitHandler);
+  phoneInput.addEventListener('input', onInputChangeHandler);
+  phoneInput.addEventListener('input', onPhoneInputChangeHandler);
+  nameInput.addEventListener('input', onInputChangeHandler);
 }
